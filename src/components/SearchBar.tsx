f42be3aa06d523abdc210ccb732a0f5787d579c2
elizzaveta <y.timchenko-2025@kpi.com>
2023-02-24 18:46:31 +0200

    Changed SearchBar appearance

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import styles from "../styles/css/components/SearchBar.module.css"
+import searchIcon from "../images/search.png"
 
 const SearchBar = () => {
     const [searchQuery, setSearchQuery] = useState<string>();
@@ -8,15 +10,29 @@ const SearchBar = () => {
         setSearchQuery(event.currentTarget.value)
     }
     const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.code === "Enter") {
+        if (event.code === "Enter" && searchQuery) {
             event.currentTarget.value = ''
             event.currentTarget.blur()
             navigation(`/?name=${searchQuery}`)
         }
     };
 
+    const handleSubmit = ()=>{
+        let search = document.getElementById("searchBar") as HTMLInputElement;
+        if(search && searchQuery){
+            search.value = '';
+            search.blur()
+            navigation(`/?name=${searchQuery}`)
+
+        }
+    }
+
+
     return (
-        <input id="searchBar" placeholder="Search..." type="text" onChange={handleChange} onKeyDown={handleKeyDown}/>
+        <div className={styles.wrapper}>
+            <input id="searchBar" className={styles.input} placeholder="Search..." type="text" onChange={handleChange} onKeyDown={handleKeyDown}/>
+            <img className={styles.searchIcon} src={searchIcon} onClick={handleSubmit}/>
+        </div>
     );
 };
 
